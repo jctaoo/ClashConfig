663630efb98f41d2feee52e5b5243d0bb9d2886c
jctaoo <jctaoo@outlook.com>
2025-10-03 05:06:44 +0800

    Refactor apply.js to improve platform-specific handling for Windows and macOS, enhance error messages, and streamline code structure.

--- apply.js
+++ apply.js
@@ -6,11 +6,14 @@ import YAML from "yaml";
 
 const code = fs.readFileSync("clash-config-script.js", "utf-8");
 
-// if windows
+// windows 使用 clash-verge-rev
 if (process.platform === "win32") {
   const appdataDir = process.env.APPDATA;
 
-  const clashVergeDir = path.join(appdataDir, "io.github.clash-verge-rev.clash-verge-rev");
+  const clashVergeDir = path.join(
+    appdataDir,
+    "io.github.clash-verge-rev.clash-verge-rev"
+  );
 
   if (!fs.existsSync(clashVergeDir)) {
     console.error(`❌ 未找到 Clash 目录: ${clashVergeDir}`);
@@ -18,7 +21,7 @@ if (process.platform === "win32") {
     process.exit(1);
   }
 
-  const scriptFile = path.join(clashVergeDir, 'profiles', "Script.js");
+  const scriptFile = path.join(clashVergeDir, "profiles", "Script.js");
 
   // 创建备份并替换脚本，提醒已经更新脚本，需要刷新订阅
   const backupFile = path.join(clashVergeDir, "Script.js.bak");
@@ -29,44 +32,38 @@ if (process.platform === "win32") {
   process.exit(0);
 }
 
+// mac 使用 clashx.meta
+if (process.platform === "darwin") {
+  const clashRelativeFolder = ".config/clash.meta/";
+  const homeDir = process.env.HOME || process.env.USERPROFILE;
 
-const clashRelativeFolder = ".config/clash.meta/";
+  const clashDir = path.join(homeDir, clashRelativeFolder);
 
-// get user home directory
-const homeDir = process.env.HOME || process.env.USERPROFILE;
+  if (!fs.existsSync(clashDir)) {
+    console.error(`❌ 未找到 Clash 目录: ${clashDir}`);
+    console.error(`请先安装 ClashX.meta`);
+    process.exit(1);
+  }
 
-// get appdata directory
-const appdataDir = process.env.APPDATA;
+  // get PROFILE from argv
+  if (process.argv.length < 3) {
+    console.error("📖 用法: update.js <配置文件名>");
+    process.exit(1);
+  }
+  const profileName = process.argv[2];
 
-const clashDir = path.join(homeDir, clashRelativeFolder);
+  const profileFile = path.join(clashDir, `${profileName}.yaml`);
 
-console.log(clashDir, appdataDir);
-process.exit(0);
+  if (!fs.existsSync(profileFile)) {
+    console.error(`❌ 未找到配置文件: ${profileFile}`);
+    process.exit(1);
+  }
 
-if (!fs.existsSync(clashDir)) {
-  console.error(`❌ 未找到 Clash 目录: ${clashDir}`);
-  process.exit(1);
-}
+  const profileContent = fs.readFileSync(profileFile, "utf-8");
+  const profile = YAML.parse(profileContent);
 
-// get PROFILE from argv
-if (process.argv.length < 3) {
-  console.error("📖 用法: update.js <配置文件名>");
-  process.exit(1);
-}
-const profileName = process.argv[2];
-
-const profileFile = path.join(clashDir, `${profileName}.yaml`);
-
-if (!fs.existsSync(profileFile)) {
-  console.error(`❌ 未找到配置文件: ${profileFile}`);
-  process.exit(1);
-}
-
-const profileContent = fs.readFileSync(profileFile, "utf-8");
-const profile = YAML.parse(profileContent);
-
-// prettier-ignore
-const run = new Function("CONFIG", "PROFILE", `
+  // prettier-ignore
+  const run = new Function("CONFIG", "PROFILE", `
   "use strict";
   let __result;
   (function(){
@@ -78,14 +75,15 @@ const run = new Function("CONFIG", "PROFILE", `
   })();
   return __result;
 `);
-const result = run(profile, profileName);
+  const result = run(profile, profileName);
 
-// backup profile file
-const backupFile = path.join(clashDir, `${profileName}.yaml.bak`);
-fs.copyFileSync(profileFile, backupFile);
+  // backup profile file
+  const backupFile = path.join(clashDir, `${profileName}.yaml.bak`);
+  fs.copyFileSync(profileFile, backupFile);
 
-// result to yaml
-const resultContent = YAML.stringify(result);
-fs.writeFileSync(profileFile, resultContent);
+  // result to yaml
+  const resultContent = YAML.stringify(result);
+  fs.writeFileSync(profileFile, resultContent);
 
-console.log("✅ 更新完成");
+  console.log("✅ 更新完成");
+}
